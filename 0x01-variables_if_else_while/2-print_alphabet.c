56c0edeccc2aa3ae8663e77f02dbfd7e10c76480
hasn422 <hasnabouali18@gmail.com>
2023-07-14 03:52:26 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,18 @@
 
 /**
  * main - prints the alphabet in lowercase,
- * followed by a new line
+ * 
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 char ch;
-
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
 putchar('\n');
-
 return (0);
 }
   
