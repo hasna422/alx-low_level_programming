e7842ccf090894f1e481e8245a9869c04226b748
hasn422 <hasnabouali18@gmail.com>
2023-07-14 03:38:43 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -12,8 +13,6 @@ for(alpha = 'a' ;alpha <= 'z' ;alpha++)
 {
 putchar(alpha);
 }
-putchar("\n");
+putchar('\n');
 return (0);
-}
-
-~  
+}  
