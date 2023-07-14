d0ff7a7980fbfe584156ff73ba8dac50963cb9ed
hasn422 <hasnabouali18@gmail.com>
2023-07-14 08:06:03 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 int n, num, nb;
-for(nb = 0; n < 8; nb++)
+for (nb = 0; nb < 8; nb++)
 {
 for (n = nb + 1; n < 9; n++)
 {
-for (num = n + 1; num < 10; num++)
+for (num = n + 1; num < 10; num+++)
 {
-putchar((nb % 10) + '0');		
+putchar((nb % 10) + '0');
 putchar((n % 10) + '0');
 putchar((num % 10) + '0');
 if (nb == 7 && n == 8 && num == 9)
@@ -26,5 +26,5 @@ putchar(' ');
 }
 }
 putchar('\n');
-return (0);
+return 0;
 }
