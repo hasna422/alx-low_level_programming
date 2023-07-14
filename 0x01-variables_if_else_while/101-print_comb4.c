ff4fa10024d09cbf98a1a2a873d1f191b36547aa
hasn422 <hasnabouali18@gmail.com>
2023-07-14 07:55:03 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n, num, nb;
+for(nb = 0; n < 8; nb++)
+{
+for (n = nb + 1; n < 9; n++)
+{
+for (num = n + 1; num < 10; num++)
+{
+putchar((nb % 10) + '0');		
+putchar((n % 10) + '0');
+putchar((num % 10) + '0');
+if (nb == 7 && n == 8 && num == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
