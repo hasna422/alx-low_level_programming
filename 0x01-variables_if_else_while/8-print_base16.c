84f620c6b777cffe09c7764c916394a3b47b3f85
hasn422 <hasnabouali18@gmail.com>
2023-07-14 07:15:42 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;	
+int n;
+for(n = 0 ; n < 10 ; n++)
+{
+putchar((n % 10) + '0');
+}
+for(ch = 'a' ; ch <= 'f' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
