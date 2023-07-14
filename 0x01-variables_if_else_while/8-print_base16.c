4bff6a64fcf2b8cbf589be8235b389b408d2175f
hasn422 <hasnabouali18@gmail.com>
2023-07-14 07:18:44 -0700

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,11 @@ int main(void)
 {
 char ch;	
 int n;
-for( n = 0 ; n < 10 ; n++)
+for(n = 0 ; n < 10 ; n++)
 {
 putchar((n % 10) + '0');
 }
-for( ch = 'a' ; ch <= 'f' ; ch++)
+for(ch = 'a' ; ch <= 'f' ; ch++)
 {
 putchar(ch);
 }
