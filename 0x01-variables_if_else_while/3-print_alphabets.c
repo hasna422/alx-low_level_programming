febf374ef232d9093a58afdafbea43f2b4913f95
hasn422 <hasnabouali18@gmail.com>
2023-07-14 04:21:25 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,13 @@
  * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);      
+putchar(ch); 
+putchar('\n');
 return (0);
 }
