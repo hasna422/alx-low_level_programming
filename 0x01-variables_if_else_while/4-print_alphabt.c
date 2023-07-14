3c2ad7dda364966094ba51643658e00845b8bfcf
hasn422 <hasnabouali18@gmail.com>
2023-07-14 06:36:24 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 char ch = 'a';
 while (ch <= 'z')
 {
-if(ch != 'e' && ch != 'q')
+if (ch != 'e' && ch != 'q')
 {
 putchar(ch);
 }
 ch++;
 }
 putchar('\n');
-return(0);
+return 0;
 }
-
