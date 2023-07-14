8b4f59a34ead95089ce8a87da6f552e5be385977
hasn422 <hasnabouali18@gmail.com>
2023-07-14 07:39:40 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n,num;
+
+for (n = 0; n < 9; n++)
+{
+for(num = n + 1 ; num < 10 ; num++)
+{
+putchar((n % 10) + '0');	
+putchar((num % 10) + '0');
+if (n == 8 && num == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
