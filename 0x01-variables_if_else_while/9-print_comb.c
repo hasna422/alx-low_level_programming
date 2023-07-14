8d9df006af4c78dad84e22b098303d2ba2da17fe
hasn422 <hasnabouali18@gmail.com>
2023-07-14 07:33:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,16 @@
 
 int main(void)
 {
-
 int n;
 
 for (n = 0; n < 10; n++)
 {
 putchar((n % 10) + '0');
-if(n == 9)
+if (n == 9)
 continue;
 putchar(',');
 putchar(' ');
 }
 putchar('\n');
-return(0);
+return (0);
 }
